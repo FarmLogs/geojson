02f1750679c9eb84f2af9705b1b8e1d7919cc618
Brad Koch <bradkoch2007@gmail.com>
2015-03-07 11:45:28 -0500

    lein init

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject geojson "0.1.0-SNAPSHOT"
+  :description "Clojure utilities for using GeoJSON."
+  :url "https://github.com/FarmLogs/geojson"
+  :dependencies [[org.clojure/clojure "1.6.0"]])
