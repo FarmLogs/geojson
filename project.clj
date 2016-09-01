4ca17945bb44017577c97a916e0ec83900e8af36
Brad Koch <brad@farmlogs.com>
2016-08-31 20:18:40 -0400

    Added deployment configuration

--- project.clj
+++ project.clj
@@ -1,5 +1,9 @@
 (defproject geojson "0.1.0-SNAPSHOT"
   :description "Clojure utilities for using GeoJSON."
   :url "https://github.com/FarmLogs/geojson"
+  :plugins [[s3-wagon-private "1.2.0"]]
+  :repositories {"farmlogs-internal" {:url "s3p://fl-maven-repo/mvn"
+                                      :username ~(System/getenv "AMAZON_KEY")
+                                      :passphrase ~(System/getenv "AMAZON_SECRET")}}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/test.check "0.7.0"]])
